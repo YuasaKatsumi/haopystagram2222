6107f5b51dd693649b9294e638fd9a74e07071a7
湯浅佑一 <yuichi427427@yahoo.co.jp>
2019-01-03 22:09:21 +0900

    Initial Commit

---
+++ Quizfirebase1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizfirebase1
+//
+//  Created by 湯浅克巳 on 2019/01/03.
+//  Copyright © 2019 湯浅克巳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
